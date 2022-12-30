76db2636d235c936b47d632c00b067bd0df487a4
volynkina <bzzz18@yandex.ru>
2022-12-30 14:30:07 +0500

    task3

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,13 @@ public class Main {
         } else if (clientOS1 == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке.");
         }
+        int year = 2021;
+        if (((year % 4==0) && (year % 100 !=0)) || (year % 400 ==0)) {
+            System.out.println(year + " год является високосным.");
+        } else {
+                System.out.println( year + " год не является високосным.");
+            }
+        }
     }
-}
 
 
