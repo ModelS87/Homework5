a38c10608bda9c761f1f04b624a5cd45294d9e9b
volynkina <bzzz18@yandex.ru>
2022-12-30 13:52:36 +0500

    task2

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,23 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int clientOS = 1;
-        if ( clientOS==0) {
+        if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
         } else {
             System.out.println("Установите версию приложения для Android по ссылке.");
         }
+        int clientOS1 = 0;
+        int clientDeviceYear = 2015;
+        if (clientOS1 == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        } else if (clientOS1 == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else if (clientOS1 == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        } else if (clientOS1 == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        }
     }
 }
+
+
