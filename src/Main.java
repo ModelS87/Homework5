d61e3725442d8478fd9a3e882b802d66ed9396c3
volynkina <bzzz18@yandex.ru>
2022-12-30 14:59:05 +0500

    task4

--- src/Main.java
+++ src/Main.java
@@ -18,13 +18,23 @@ public class Main {
         } else if (clientOS1 == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке.");
         }
-        int year = 2021;
-        if (((year % 4==0) && (year % 100 !=0)) || (year % 400 ==0)) {
+        int year = 1964;
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
             System.out.println(year + " год является високосным.");
         } else {
-                System.out.println( year + " год не является високосным.");
-            }
+            System.out.println(year + " год не является високосным.");
+        }
+        int deliveryDistance = 95;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется 1 день.");
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуется 2 дня.");
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется 3 дня.");
+        } else {
+            System.out.println("Доставки нет.");
         }
     }
+}
 
 
