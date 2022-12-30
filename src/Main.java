ace97594839cfbb73d3aa7a229c2cc3302291597
volynkina <bzzz18@yandex.ru>
2022-12-30 15:28:47 +0500

    task5

--- src/Main.java
+++ src/Main.java
@@ -34,6 +34,47 @@ public class Main {
         } else {
             System.out.println("Доставки нет.");
         }
+        int monthNumber = 7;
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Месяц " + monthNumber + " принадлежит сезону зима.");
+                break;
+            case 2:
+                System.out.println("Месяц " + monthNumber + " принадлежит сезону зима.");
+                break;
+            case 3:
+                System.out.println("Месяц " + monthNumber + " принадлежит сезону весна.");
+                break;
+            case 4:
+                System.out.println("Месяц " + monthNumber + " принадлежит сезону весна.");
+                break;
+            case 5:
+                System.out.println("Месяц " + monthNumber + " принадлежит сезону весна.");
+                break;
+            case 6:
+                System.out.println("Месяц " + monthNumber + " принадлежит сезону лето.");
+                break;
+            case 7:
+                System.out.println("Месяц " + monthNumber + " принадлежит сезону лето.");
+                break;
+            case 8:
+                System.out.println("Месяц " + monthNumber + " принадлежит сезону лето.");
+                break;
+            case 9:
+                System.out.println("Месяц " + monthNumber + " принадлежит сезону осень.");
+                break;
+            case 10:
+                System.out.println("Месяц " + monthNumber + " принадлежит сезону осень.");
+                break;
+            case 11:
+                System.out.println("Месяц " + monthNumber + " принадлежит сезону осень.");
+                break;
+            case 12:
+                System.out.println("Месяц " + monthNumber + " принадлежит сезону зима.");
+                break;
+            default:
+                System.out.println("Такого месяца не существует.");
+        }
     }
 }
 
